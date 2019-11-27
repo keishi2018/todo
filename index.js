6a8a6cc1131519ab3d03f7a589c51849b926f114
KC <kckc@KCnoMacBook-Pro.local>
2019-11-27 10:51:09 +0900

    P224まで終了

--- index.js
+++ index.js
@@ -5,15 +5,8 @@ var addZero = function(value) {
     return value;
 };
 
+document.getElementById('start_stop').addEventListener('click', function() {
+    var start = new Date();
 
-var now = new Date(); //dataオブジェクトのインスタンスを作る
-var seconds = now.getSeconds(); //秒を取得する
-seconds = addZero(seconds); //addZeroファンクションを利用
-
-var minutes = now.getMinutes(); //分を取得
-minutes = addZero(minutes); //addZeroファンクションを利用
-
-var hours = now.getHours(); //時を取得する
-hours = addZero(hours); //addZeroファンクションを利用
-
-document.getElementById('timer').innerHTML = hours + ':' + minutes + ':' + seconds;
+    document.getElementById('timer').innerHTML = start.getTime();
+});
