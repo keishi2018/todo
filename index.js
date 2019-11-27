5cec73a3fe336350e221b109654b0e838f3acd96
KC <kckc@KCnoMacBook-Pro.local>
2019-11-27 10:18:22 +0900

    218Pまで

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-var now = new Date();
-var seconds = now.getSeconds();
+var addZero = function(value) {
+    if (value < 10) {
+        value = '0' + value;
+    }
+    return value;
+};
 
-if(seconds < 10){
-  seconds = '0' + seconds;
-}
 
-document.getElementById('timer').innerHTML = seconds;
+var now = new Date(); //dataオブジェクトのインスタンスを作る
+var seconds = now.getSeconds(); //秒を取得する
+seconds = addZero(seconds); //addZeroファンクションを利用
 
+var minutes = now.getMinutes(); //分を取得
+minutes = addZero(minutes); //addZeroファンクションを利用
+
+var hours = now.getHours(); //時を取得する
+hours = addZero(hours); //addZeroファンクションを利用
+
+document.getElementById('timer').innerHTML = hours + ':' + minutes + ':' + seconds;
